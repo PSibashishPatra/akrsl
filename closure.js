2172f8b2c4f1fde4e2c08329cd0abb2581b34fcb
PSibashishPatra <psibashishpatra@gmail.com>
2023-11-21 15:33:11 +0530

    first commit

--- closure.js
+++ closure.js
@@ -1,11 +1,44 @@
 function x() {
     var a = 12 ;
-    let b = 15 ;
+    //let b = 15 ;
     function y() {
         console.log(a);
-        console.log(b);
+      //  console.log(b);
     }
-    y();
-
+   // y();
+    return y;
 }
 x();
+var n = x();
+console.log(n);
+n();
+
+
+
+function l() {
+    var p = 4;
+    function m() {
+        console.log(p);
+    }
+    p = 100
+    return m ;
+
+}
+var z = l();
+console.log(z);
+z();
+
+
+function s() {
+    var f = 55 ;
+    function t() {
+        var e = 65 ;
+        function u() {
+            console.log(e,f,j);
+        }
+        u();
+    }
+    t();
+}
+let j = 44;
+s();
